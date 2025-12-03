7ba5603d6312ac32a693e4bafe52d40e702e4c10
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-12-03 09:01:16 +0100

    Initial project structure

---
+++ src/main/java/com/medical/app/MedicalAppointmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medical.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentBackendApplication.class, args);
+    }
+
+}
